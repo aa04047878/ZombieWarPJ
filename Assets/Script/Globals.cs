6b8b907a82bea69fa399eb06fb987d9db839b3a0
aa04047878 <aa04047878@gmail.com>
2025-06-11 15:35:56 +0800

    update DeleteCode
    把用不到的代碼砍掉

--- Assets/Script/Globals.cs
+++ Assets/Script/Globals.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class Globals 
 {
     // 背景音樂
-    public const string BGM1 = "Audio/Music/bgm1";
-    public const string BGM2 = "Audio/Music/bgm2";
-    public const string BGM3 = "Audio/Music/bgm3";
-    public const string BGM4 = "Audio/Music/bgm4";
-    public const string BGM5 = "Audio/Music/bgm5";
-    public const string BGM6 = "Audio/Music/ThemeSong";
+    
     public const string LoadingMusic = "Audio/Music/LoadingMusic";
     public const string MainMenuMusic = "Audio/Music/MainMenuMusic";
     public const string BattleMusic = "Audio/Music/BattleMusic";
@@ -38,100 +33,12 @@ public class Globals
     public const string PurpleMushroomAllbody = "Image/AllBody/PurpleMushroomAllbody";
 
     /// <summary>
-    /// 準備好開始
-    /// </summary>
-    public const string S_Relllsetplant = "Audio/Sound/readysetplant";
-
-    /// <summary>
-    /// 陽光撿取
-    /// </summary>
-    public const string S_SunCollect = "Audio/Sound/points";
-    /// <summary>
-    /// 豌豆射擊
-    /// </summary>
-    public const string S_Shoot = "Audio/Sound/shoot";
-    /// <summary>
-    /// 豌豆射擊到身上
-    /// </summary>
-    public const string S_PeaHit = "Audio/Sound/kernelpult2";
-    /// <summary>
-    /// 殭屍開始出現
-    /// </summary>
-    public const string S_TheZombiesAreComing = "Audio/Sound/awooga";
-    /// <summary>
-    /// 小偷殭屍
-    /// </summary>
-    public const string S_SharpZombieCombing = "Audio/Sound/bungee_scream";
-    /// <summary>
-    /// 核彈
-    /// </summary>
-    public const string S_Doomshroom = "Audio/Sound/doomshroom";
-    /// <summary>
-    /// 殭屍叫聲
-    /// </summary>
-    public const string S_ZombieSound1 = "Audio/Sound/groan";
-    /// <summary>
-    /// 殭屍吃
-    /// </summary>
-    public const string S_ZombieEat = "Audio/Sound/chompsoft";
-
-    /// <summary>
     /// 種植 
     /// </summary>
     public const string S_Plant = "Audio/Sound/plant";
-    /// <summary>
-    /// 一大波殭屍
-    /// </summary>
-    public const string S_Hugewave = "Audio/Sound/hugewave";
-    /// <summary>
-    /// 最後一波
-    /// </summary>
-    public const string S_Finalwave = "Audio/Sound/finalwave";
-    /// <summary>
-    /// 辣椒
-    /// </summary>
-    public const string S_Jalapeno = "Audio/Sound/jalapeno";
-    /// <summary>
-    /// 小推車
-    /// </summary>
-    public const string S_CAR = "Audio/Sound/lawnmower";
-    /// <summary>
-    /// 失敗
-    /// </summary>
-    public const string S_Lost = "Audio/Sound/losemusic";
-    /// <summary>
-    /// 二爺
-    /// </summary>
-    public const string S_Newspaper_Rarrgh = "Audio/Sound/newspaper_rarrgh";
-    public const string S_NeLbeeper_Rip = "Audio/Sound/newspaper_rip";
-    /// <summary>
-    /// 花生地雷
-    /// </summary>
-    public const string S_Potato_Mine = "Audio/Sound/potato_mine";
+
     /// <summary>
     /// 選擇卡片
     /// </summary>
     public const string S_Seedlift = "Audio/Sound/seedlift";
-    /// <summary>
-    /// 打擊鐵桶
-    /// </summary>
-    public const string S_Shieldhit = "Audio/Sound/shieldhit";
-    /// <summary>
-    /// 鐵鏟
-    /// </summary>
-    public const string S_Shovel = "Audio/Sound/shovel";
-    /// <summary>
-    /// 勝利
-    /// </summary>
-    public const string S_Winmusic = "Audio/Sound/winmusic";
-    /// <summary>
-    /// 火焰
-    /// </summary>
-    public const string S_Fire = "Audio/Sound/frozen";
-    /// <summary>
-    /// 戴夫
-    /// </summary>
-    public const string S_DAIFU_FINAL = "Audio/Sound/Daifu/Crazydavescream";
-    public const string S_DAIFU1 = "Audio/Sound/Daifu/Crazydaveshort1";
-    public const string S_DAIFU2 = "Audio/Sound/Daifu/Crazydaveshort2";
 }
